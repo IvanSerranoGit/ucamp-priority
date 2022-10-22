4d72a5321826f23300c72875b314c460b7be0413
Iván Serrano <contacto.ivanserrano@gmail.com>
2022-10-21 22:17:31 -0500

    se crea la logica de la app, tambien se crea codigo que trae la hora y dia

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var prioridades = [];
+
+var misPrioridades = document.getElementById('monstrarPrioridades');
+var prioridad = document.getElementById('prioridad');
+
+var mostrar = document.getElementById('mostrar');
+
+if(prioridades.length == 0){
+    misPrioridades.innerHTML = "Aún no has agregado ninguna prioridad"
+}
+
+function pushButton() {
+    prioridades.push(prioridad.value);
+    misPrioridades.innerHTML =  ` ${prioridades}`;
+    let json = JSON.stringify(prioridades);
+    let local = localStorage.setItem("data", json);
+    console.log(local);
+}
+function popButton() {
+    prioridades.pop()
+    misPrioridades.innerHTML =  ` ${prioridades}`;
+}
+function shiftButton() {
+    prioridades.shift()
+    misPrioridades.innerHTML =  ` ${prioridades}`;
+}
+let date = new Date().toDateString();
+let fecha = document.getElementById('date');
+fecha.innerHTML =  ` ${date}`;
+
+// let params = new URLSearchParams({
+//     access_key: 'f5c083ec9d8b9da800761e01757f67b1',
+//     query: 'Mexico'
+// })
+// const URL = `http://api.weatherstack.com/current${params}`;
+// fetch(URL)
+// .then(res => res.json())
+// .then(console.log())
+
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
